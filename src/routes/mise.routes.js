d67c9378abf1502987cddfd9ec6ea998913c847c
dynepark9111 <dynepark9111@gmail.com>
2022-07-10 16:56:25 +0900

    build folder testing

--- src/routes/mise.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-
-const miseController = require("../controllers/mise.controller");
-
-router.get("/", miseController.getTest);
-router.get("/:stationName", miseController.getMise);
-
-module.exports = router;
