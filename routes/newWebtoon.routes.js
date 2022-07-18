3cf5f2649a3a8791c2ea665230335290268afc08
dynepark9111 <dynepark9111@gmail.com>
2022-07-18 19:12:35 +0900

    basic express app testing

--- routes/newWebtoon.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-
-const newController = require("../controllers/newWebtoon.controller");
-
-router.get("/:platform", newController.getNewWebtoons);
-
-module.exports = router;
